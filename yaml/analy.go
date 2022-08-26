33fd104583bf4800f35d6da8f5b190f89ebfad65
swxctx <swxctx@gmail.com>
2022-08-26 17:08:36 +0800

    fix yaml config

--- yaml/analy.go
+++ yaml/analy.go
@@ -1,15 +1,10 @@
 package yaml
 
 import (
-	"github.com/mitchellh/mapstructure"
 	"gopkg.in/yaml.v2"
 )
 
 // Unmarshal 解析yaml
 func Unmarshal(data []byte, res interface{}) error {
-	var (
-		conf interface{}
-	)
-	yaml.Unmarshal(data, &conf)
-	return mapstructure.Decode(conf, res)
+	return yaml.Unmarshal(data, res)
 }
